7495333fc05d787bcf1f7bcb2a0c471d9014a384
Renzo Lucioni <renzo@renzolucioni.com>
2015-11-10 13:25:35 -0500

    WIP: Add Programs tab to Studio
    Extends the Programs ConfigurationModel. Uses the Programs API to list programs within Studio. ECOM-2769.

---
+++ openedx/core/djangoapps/programs/tests/test_utils.py
@@ -0,0 +1,1 @@
+"""Tests covering Programs utilities."""
--- openedx/core/djangoapps/programs/utils.py
+++ openedx/core/djangoapps/programs/utils.py
@@ -1,15 +1,15 @@
 """
 Helper methods for Programs.
 """
+import logging
+
 from edx_rest_api_client.client import EdxRestApiClient
+
 from openedx.core.djangoapps.programs.models import ProgramsApiConfig
+from openedx.core.djangoapps.util.helpers import get_id_token
 
 
-def is_student_dashboard_programs_enabled():    # pylint: disable=invalid-name
-    """ Returns a Boolean indicating whether LMS dashboard functionality
-     related to Programs should be enabled or not.
-    """
-    return ProgramsApiConfig.current().is_student_dashboard_enabled
+log = logging.getLogger(__name__)
 
 
 def programs_api_client(api_url, jwt_access_token):
@@ -20,3 +20,79 @@ def programs_api_client(api_url, jwt_access_token):
         api_url,
         jwt=jwt_access_token
     )
+
+
+def get_programs(user):
+    """Given a user, get programs from the Programs service.
+
+    Arguments:
+        user (User): The user to authenticate as when requesting programs.
+
+    Returns:
+        list of dict, representing programs returned by the Programs service.
+    """
+    programs_config = ProgramsApiConfig.current()
+    no_programs = []
+
+    if not programs_config.enabled:
+        log.warning('Programs configuration is disabled.')
+        return no_programs
+
+    try:
+        jwt = get_id_token(user, programs_config.OAUTH2_CLIENT_NAME)
+        api = EdxRestApiClient(programs_config.internal_api_url, jwt=jwt)
+    except Exception:  # pylint: disable=broad-except
+        log.exception('Failed to initialize the Programs API client.')
+        return no_programs
+
+    try:
+        response = api.programs.get()
+    except Exception:  # pylint: disable=broad-except
+        log.exception('Failed to retrieve programs from the Programs API.')
+        return no_programs
+
+    return response.get('results', no_programs)
+
+
+def get_programs_for_dashboard(user, course_keys):
+    """Build a dictionary of programs, keyed by course.
+
+    Given a user and an iterable of course keys, find all the programs relevant
+    to the user's dashboard and return them in a dictionary keyed by course key.
+
+    Arguments:
+        user (User): The user to authenticate as when requesting programs.
+        course_keys (list): List of course keys representing the courses in which
+            the given user has active enrollments.
+
+    Returns:
+        dict, containing programs keyed by course. Empty if programs cannot be retrieved.
+    """
+    programs_config = ProgramsApiConfig.current()
+    course_programs = {}
+
+    if not programs_config.is_student_dashboard_enabled:
+        log.warning('Display of programs on the student dashboard is disabled.')
+        return course_programs
+
+    programs = get_programs(user)
+    if not programs:
+        log.warning('No programs found for the user with ID %d.', user.id)
+        return course_programs
+
+    # Reindex the result returned by the Programs API from:
+    #     program -> course code -> course run
+    # to:
+    #     course run -> program
+    # Ignore course runs not present in the user's active enrollments.
+    for program in programs:
+        try:
+            for course_code in program['course_codes']:
+                for run in course_code['run_modes']:
+                    course_key = run['course_key']
+                    if course_key in course_keys:
+                        course_programs[course_key] = program
+        except KeyError:
+            log.exception('Unable to parse Programs API response: %r', program)
+
+    return course_programs
