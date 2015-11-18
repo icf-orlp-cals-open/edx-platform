cd1a06a5ce8f620416ca3d2d771f181c35db2eb4
Chris Dodge <cdodge@edx.org>
2015-11-18 15:01:03 -0500

    fix test

--- lms/djangoapps/instructor_task/tests/test_tasks_helper.py
+++ lms/djangoapps/instructor_task/tests/test_tasks_helper.py
@@ -419,7 +419,7 @@ class TestInstructorDetailedEnrollmentReport(TestReportMixin, InstructorTaskCour
         with patch('instructor_task.tasks_helper._get_current_task'):
             result = upload_enrollment_report(None, None, self.course.id, task_input, 'generating_enrollment_report')
 
-        enrollment_source = u'manually enrolled by {username} - reason: manually enrolling unenrolled user'.format(
+        enrollment_source = u'manually enrolled by username: {username}'.format(
             username=self.instructor.username)  # pylint: disable=no-member
         self.assertDictContainsSubset({'attempted': 1, 'succeeded': 1, 'failed': 0}, result)
         self._verify_cell_data_in_csv(student.username, 'Enrollment Source', enrollment_source)
